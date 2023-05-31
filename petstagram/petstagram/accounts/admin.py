8f4c7dbf42afc4a05cd0e97acd79f5f74e1f428a
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-05-31 17:36:35 +0300

    Some changes

--- petstagram/petstagram/accounts/admin.py
+++ petstagram/petstagram/accounts/admin.py
@@ -3,7 +3,15 @@ from django.contrib.auth.models import User
 
 
 class MyUserAdmin(admin.ModelAdmin):
-    fields = [('first_name', 'last_name'), ('username', 'password'), 'email']
+    # fields = [('username', 'password'), ('first_name', 'last_name'), 'email']
+    search_fields = ['first_name', 'last_name', 'username', 'email']
+    list_filter = ['username', 'first_name', 'last_name']
+    fieldsets = (
+        ('Login info',
+         {'fields': ('username', 'password')}),
+        ('Personal info',
+         {'fields': ('first_name', 'last_name', 'email')}),
+    )
 
 
 admin.site.unregister(User)
